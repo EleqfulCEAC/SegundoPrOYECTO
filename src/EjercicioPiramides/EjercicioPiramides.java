a089f1f998ee3347538d589c98afb35181925848
EleanFP <elean.quintero@alu.ceacfp.es>
2024-10-24 11:59:46 +0200

    ejercicios clase 24/10/2024

--- src/EjercicioPiramides/EjercicioPiramides.java
+++ src/EjercicioPiramides/EjercicioPiramides.java
@@ -5,17 +5,40 @@ import java.util.Scanner;
 public class EjercicioPiramides {
 
 	public static void main(String[] args) {
-		Scanner caracter = new Scanner(System.in);
-		Scanner numDeVueltas = new Scanner (System.in);
+		//piramide(6, "*");
+		piramideEquilatera(5, "x");
+	
+	}
+	
+	public static void piramide (int altura, String caracter) {
 		
-		System.out.println("introduce un caracter");
-		String MyCharacter =  caracter.next();
-		System.out.println("introduce el numero de altura");
-		int MynumDeVueltas = numDeVueltas.nextInt();
-		int contador = 1;
+		String txt="";
+		for (int i = 1; i <= altura; i++) {
+			txt += caracter; 
+			System.out.println(txt);
+		}
+		
+		for (int i = altura -1; i >= 0; i--) {
+			txt = txt.substring(0,i);
+			System.out.println(txt);
+		}
+		
+	}
+	
+	public static void piramideEquilatera(int altura, String caracter) {
+		int espacios = altura -1; 
+		String txt = caracter; 
+		for (int i = 0; i < altura; i++) {
+			for (int x = 0; x < espacios; x++) {
+				System.out.print(" ");	
+			} 
+			System.out.println(txt);
+			txt += caracter + caracter; 
+			espacios--;
+			
+		}
 		
 		
-
 	}
 
 }
